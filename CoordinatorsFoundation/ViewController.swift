24ae8b67a1c7704c34a7dfd81d6de36949c8c2bc
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2019-09-04 09:47:24 +0200

    Created project & README

---
+++ CoordinatorsFoundation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorsFoundation
+//
+//  Created by Bartłomiej Świerad on 04/09/2019.
+//  Copyright © 2019 Bartłomiej Świerad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoordinatorsFoundationTests/CoordinatorsFoundationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoordinatorsFoundationTests.swift
+//  CoordinatorsFoundationTests
+//
+//  Created by Bartłomiej Świerad on 04/09/2019.
+//  Copyright © 2019 Bartłomiej Świerad. All rights reserved.
+//
+
+import XCTest
+@testable import CoordinatorsFoundation
+
+class CoordinatorsFoundationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
