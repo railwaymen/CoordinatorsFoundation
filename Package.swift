5c2fc2481a184305fbddee358031a70d002a9c54
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-05-22 11:38:12 +0200

    Added bundler and needed gems

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -15,8 +13,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CoordinatorsFoundation",
             dependencies: []),
