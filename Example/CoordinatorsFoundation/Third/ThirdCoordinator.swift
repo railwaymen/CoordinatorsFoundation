bdc71025a34c3949683d57d4ad426d75936e35d0
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-05-26 12:13:37 +0200

    Removed linking of the framework in Example app using CocoaPods - linked with SPM

--- Example/CoordinatorsFoundation/Third/ThirdCoordinator.swift
+++ Example/CoordinatorsFoundation/Third/ThirdCoordinator.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Railwaymen. All rights reserved.
 //
 
+import UIKit
 import CoordinatorsFoundation
 
 typealias ThirdCoordinatorChild = NavigationCoordinator & TabBarChildCoordinatorType
